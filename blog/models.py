3dff46f23ac5b2b8996746ba66083cf70d100274
Joey Reiness <j.reiness@gmail.com>
2019-02-21 16:52:45 +0000

    lesson 5 finished

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here. Models are written as Python classes. 
+# Each class will be its own table in the database.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #on_delete says what to do if user is deleted - what happens to post?
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,15 @@
 #This is where the logic goes for how we want to handle certain routes
 
 from django.shortcuts import render
+from .models import Post
 
-#fake date for now
-posts = [
-    {
-        'author': 'ReinessJ',
-        'title': 'My first blog post',
-        'content': 'First content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'DoeJ',
-        'title': 'OMG!',
-        'content': 'Second content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     """ Returns a HTTP request or exception"""
     # Define this dictionary key to above fake data so that in home.html 
     # we can refer to the above dictionary as 'posts'
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
