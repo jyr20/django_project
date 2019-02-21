e105235d17654e73ef653544c86fce02bc64148b
Joey Reiness <j.reiness@gmail.com>
2019-02-21 16:06:56 +0000

    lesson 4 finished

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
 #This is where the logic goes for how we want to handle certain routes
 
+from django.shortcuts import render
+
+#fake date for now
+posts = [
+    {
+        'author': 'ReinessJ',
+        'title': 'My first blog post',
+        'content': 'First content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'DoeJ',
+        'title': 'OMG!',
+        'content': 'Second content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    '''Return a http response that says we're on the home page'''
-    return HttpResponse('<h1>Blog Home</h1>')
+    """ Returns a HTTP request or exception"""
+    # Define this dictionary key to above fake data so that in home.html 
+    # we can refer to the above dictionary as 'posts'
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
